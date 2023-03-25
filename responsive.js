759f32763b1608b9242527de7963d973a5bfcd1b
Mário Mutondo <74879760+Mario-mutondo@users.noreply.github.com>
2023-03-25 12:47:15 +0000

    second rectangle

--- responsive.js
+++ responsive.js
@@ -8,26 +8,42 @@ gsap.to(".rect", {
     yoyo: true,
 })
 
-
-let parent = document.querySelector(".container")
+/***************************************************************/
+/***************************************************************/
+let parent = document.querySelector('.container')
 gsap.defaults({
-    duration: 2, 
-    ease: "power2.inout",
+  duration: 2, ease: "power2.inOut"
 })
 
-//animate across a parent, easy enought but it does not update when the screen size change 
-let tween = gsap.to(".green", {
-    x: () => parent.clientWidth,
-    xpercent: -100,
+// animate across a parent. Easy enough, but it doesn't update when the screen size changes
+let tween = gsap.to('.green', {
+  x: () => parent.clientWidth,
+  xPercent: -100
 })
 
-//we can update the tween after the has resized
-function CallAfterResize(func, delay){
-    let dc = gsap.delayedcall(delay || 0.2, func).pause(),
-    handler = dc.restart(true);
-    window.addEventListener("resize", handler);
-    return handler; //in case you want to window.removeEventListener later
+// we can update the tween after the page has resized
+function callAfterResize(func, delay) {
+  let dc = gsap.delayedCall(delay || 0.2, func).pause(),
+    handler = () => dc.restart(true);
+  window.addEventListener("resize", handler);
+  return handler; // in case you want to window.removeEventListener() later
 }
 
-//but it feel a bit messy 
-CallAfterResize = ()
+// but it feels a bit messy
+callAfterResize(() => {
+  tween.invalidate().restart().progress(1).pause()
+}); 
+
+// A better approach is to put the element in the end position and use a 'from' tween to animate 'from' that transformed position
+
+// avoid FOUC by setting the opacity 0 in CSS. Careful to make sure you're falling back to visible content for users without JS!
+// https://greensock.com/fouc/
+gsap.set('.blue', {opacity: 1});
+
+gsap.from('.blue', {
+  x: () => -1 * parent.clientWidth,
+  xPercent: 100
+})
+/***************************************************************/
+/***************************************************************/
+
